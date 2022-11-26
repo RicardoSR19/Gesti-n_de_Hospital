1eb28adb501073ec58ecde41775d8c690340a725
RicardoSR19 <117595328+RicardoSR19@users.noreply.github.com>
2022-11-26 16:38:53 -0600

    Update Manejo_Archivo.h

--- Gestion_Hospital_Codigo/Manejo_Archivo.h
+++ Gestion_Hospital_Codigo/Manejo_Archivo.h
@@ -1,7 +1,7 @@
 #include <fstream>
 
 class Manejo_Archivo{ //Clase
-    public: //M�todos
+    public: //Metodos
         void guardarArchivo(Paciente[], Personal_administracion[], Personal_limpieza[], Personal_transporte[], Medico[]); // Función para guardar archivo
 
 };
